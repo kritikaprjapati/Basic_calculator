b54cd1fb412b10a388c186ad9eb4572d88622ee1
kritikaprjapati <146057641+kritikaprjapati@users.noreply.github.com>
2025-07-31 22:23:24 +0530

    Add files via upload

---
+++ Basic calculator/script.js
@@ -0,0 +1,19 @@
+function appendvalue(value){
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay(){
+    document.getElementById('display').value = "";
+}
+
+function calculate(){
+    try{
+        const result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    }
+    catch (error) {
+    alert("Invalid expression");
+    clearDisplay();
+  }
+    
+}
